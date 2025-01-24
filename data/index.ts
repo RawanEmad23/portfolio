450b61e8fc4afcac677b117bd8d19c70647cd37e
RawanEmad23 <re0999578@gmail.com>
2025-01-24 21:15:44 +0200

    Added other files

---
+++ data/index.ts
@@ -0,0 +1,57 @@
+export const navItems = [
+ 
+  { name: "Projects", link: "projects" },
+
+];
+
+
+export const projects = [
+  {
+    id: 1,
+    title: "React E-commerce Project",
+    des: "This is an e-commerce React project with amazing functionality.",
+    img: "/1.jpg",
+    iconLists: ["/re.svg"],
+    link: "https://e-commerce-rho-cyan.vercel.app/login", 
+    githubLink: "https://github.com/RawanEmad23/E-commerce",
+  },
+  {
+    id: 2,
+    title: "Farm Management System",
+    des: "Comprehensive farm management system built with React for efficiency.",
+    img: "/2.jpg",
+    iconLists: ["/re.svg"],
+    link: "https://farm-mangment.vercel.app/login", 
+    githubLink: "https://github.com/RawanEmad23/Farm-Mangment", 
+  },
+  {
+    id: 3,
+    title: "Company Portfolio Website",
+    des: "Company portfolio website built with Next.js, showcasing services.",
+    img: "/3.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://webbing-agency.com/",
+    githubLink: "https://github.com/RawanEmad23/Webbing-Agency", 
+  },
+  {
+    id: 4,
+    title: "Adil Baba Dashboard",
+    des: "Modern dashboard for Adil Baba with Next.js, showcasing dynamic data.",
+    img: "/5.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://adil-baba.vercel.app/dashboard", 
+    githubLink: "https://github.com/RawanEmad23/Adil-Baba", 
+  },
+  {
+    id: 5,
+    title: "JavaScript Project",
+    des: "An innovative JavaScript project demonstrating core functionality.",
+    img: "/4.jpg",
+    iconLists: [],
+    link: "https://rawanemad23.github.io/FoodApp/", 
+    githubLink: "https://github.com/RawanEmad23/FoodApp", 
+  },
+];
+
+
+
